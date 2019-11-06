9a61f2382e3a8a56c3ab3035cf17a0934c3ea817
Kha Do <kha.do@quoine.com>
2019-11-06 10:53:22 +0700

    Add sample config

---
+++ config.sample.php
@@ -0,0 +1,14 @@
+<?php
+// Database config
+$DB_HOST     = 'localhost';
+$DB_NAME     = 'demo1';
+$DB_USER     = 'root';
+$DB_PASSWORD = 'password';
+
+// Email config
+$EMAIL_FROM     = 'abcyxz@gmail.com';
+$EMAIL_NAME     = 'LT Web 1 - 2019';
+$EMAIL_PASSWORD = 'password';
+
+// Base url config
+$BASE_URL = 'http://localhost:8888/BTCN08';
